c4217e8959f6e739498f353326dc3791c53fbc77
parkmuhyeun <pjhg410@gmail.com>
2024-03-23 17:52:17 +0900

    fix: index명 수정

--- javaElasticSearch/src/main/java/com/hackaton/javaelasticsearch/domain/elasticsearch/delete/model/DeleteVector.java
+++ javaElasticSearch/src/main/java/com/hackaton/javaelasticsearch/domain/elasticsearch/delete/model/DeleteVector.java
@@ -11,7 +11,7 @@ import org.springframework.data.elasticsearch.annotations.FieldType;
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
-@Document(indexName = "text_vector_index")
+@Document(indexName = "pdf_index")
 
 public class DeleteVector {
     @Id
